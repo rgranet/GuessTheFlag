03b887495799754c8621cc9e27428e9d1d2b2db7
Ruben Granet <ruben.granet@icloud.com>
2022-11-27 22:43:42 +0200

    Add an end game
    Add an end game alert
    Add a reset function to reset all the game at the end game
    Max 8 questions for now.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "USA"].shuffled()
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
+    @State private var endGame = false
+    @State private var numberOfQuestion = 0
     
     var body: some View {
         ZStack {
@@ -74,23 +76,43 @@ struct ContentView: View {
         } message: {
             Text("Your score is \(score)")
         }
-        
+        .alert(scoreTitle, isPresented: $endGame) {
+            Button("Reset the game", action: reset)
+        } message: {
+            Text("Your score is \(score) / \(numberOfQuestion + 1)")
+        }
     }
     
     func flagTapped(_ number: Int) {
-        if number == correctAnswer {
+        let maxQuestions = 7
+        switch numberOfQuestion {
+        case maxQuestions: endGame = true
+            scoreTitle = "This is the end of the game"
+        
+        default:  if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            numberOfQuestion += 1
         } else {
             scoreTitle = "Wrong! \nThis is the flag of \(countries[number])"
+            numberOfQuestion += 1
         }
         
         showingScore = true
+        }
+        
     }
     
     func askQuestion() {
         countries = countries.shuffled()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        countries = countries.shuffled()
+        endGame = false
+        numberOfQuestion = 0
+        score = 0
     }
     
 }
